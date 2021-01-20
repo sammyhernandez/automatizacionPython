8fd6c8144460e7ad165fed2459f80e3fa302af9e
SammyHernandez <Master-pro@windowslive.com>
2021-01-20 14:55:24 -0400

    E2_ingresar_datos_de_correos
    Ejercicios #2

---
+++ E2_ingresar_datos_de_correos.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome(executable_path=r"C:\ChromeDriver\chromedriver.exe")
+driver.get("https://gmail.com")
+
+
+usuario = driver.find_element_by_id("identifierId")
+usuario.send_keys("sammy181090@gmail.com")
+usuario.send_keys(Keys.ENTER)
+time.sleep(3)
+
+contrasenia = driver.find_element_by_name("password")
+contrasenia.send_keys("00000000")
+contrasenia.send_keys(Keys.ENTER)
+time.sleep(3)
+
+driver.close()
